3550ebccce2ef704bf38901976c78b032eae3ce6
jacknicosia <jackrileynicosia@yahoo.com>
2023-07-18 14:25:43 -0400

    gameboard is hidden until play button is clicked

--- script.js
+++ script.js
@@ -22,46 +22,68 @@ var gameboard = (function () {
 
   const box0 = document.createElement("div");
   box0.id = "box0";
-  document.body.appendChild(box0);
-
   const box1 = document.createElement("div");
   box1.id = "box1";
-  document.body.appendChild(box1);
-
   const box2 = document.createElement("div");
   box2.id = "box2";
-  document.body.appendChild(box2);
-
   const box3 = document.createElement("div");
   box3.id = "box3";
-  document.body.appendChild(box3);
-
   const box4 = document.createElement("div");
   box4.id = "box4";
-  document.body.appendChild(box4);
-  
   const box5 = document.createElement("div");
   box5.id = "box5";
-  document.body.appendChild(box5);
-
   const box6 = document.createElement("div");
   box6.id = "box6";
-  document.body.appendChild(box6);
-
   const box7 = document.createElement("div");
   box7.id = "box7";
-  document.body.appendChild(box7);
-
   const box8 = document.createElement("div");
   box8.id = "box8";
-  document.body.appendChild(box8);
 
-  const boxArray = [box0, box1, box2, box3, box4];
+
+  const boxArray = [box0, box1, box2, box3, box4, box5, box6, box7, box8];
+
+
+  
+  const boxArrayContainer = document.querySelector(".boxArray");
+
+
+
+
+  // Append each box to the "boxArray" div
+  boxArray.forEach(box => {
+    boxArrayContainer.appendChild(box);
+  });
+  
+ 
+
+
+    // Return the boxArray so it can be accessed outside the IIFE
   return {
+    getBoxArray: function() {
+      return boxArray;
+    }
   
   };
 
-
-
 })();
-  
+
+// Now you can access the boxArray outside the IIFE
+const boxes = gameboard.getBoxArray();
+console.log(boxes); // Array of div elements (box0 to box8)
+
+
+const element = document.getElementById("box4");
+element.addEventListener("click", myFunction);
+
+function myFunction() {
+  document.getElementById("box4").innerHTML = "X";
+}
+
+
+const playButton = document.getElementById("play");
+playButton.addEventListener("click", showBoxArray);
+
+function showBoxArray(){
+  const boxArrayElement = document.querySelector(".boxArray");
+  boxArrayElement.style.display = "grid";
+}
