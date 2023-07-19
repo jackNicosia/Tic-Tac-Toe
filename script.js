98476482fd857c6a29690e9e429c8119b524d041
jacknicosia <jackrileynicosia@yahoo.com>
2023-07-19 15:31:00 -0400

    colored symbols and dispaly current player

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
-
 // create gameboard and display controller with modules
 // create players with factories 
-
-
-// const gameboardArray = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-
-
 
 function createPlayer(name, symbol) {
   return {
@@ -20,10 +14,16 @@ function createPlayer(name, symbol) {
 let player1 = createPlayer("Player 1", "X");
 let player2 = createPlayer("Player 2", "O");
 
-console.log(player1.getPlayerId());
-console.log(player2.getPlayerId());
-
-
+const handleClick = function() {
+  if (!this.innerHTML) {
+    this.innerHTML = currentPlayer.symbol;
+    if (currentPlayer === player1) {
+      this.classList.add("player1-symbol");
+    }
+    currentPlayer = currentPlayer === player1 ? player2 : player1; // Switch players
+    updatePlayerTurnMessage(); // Update the player turn message
+  }
+}
 
 var gameboard = (function () {
   'use strict';
@@ -44,9 +44,11 @@ var gameboard = (function () {
   const box7 = document.createElement("div");
   box7.className = "gray-box";
   const box8 = document.createElement("div");
-  box8.className  = "blue-box";
+  box8.className = "blue-box";
   const resetButton = document.createElement("button");
   resetButton.id = "reset-button";
+  var playerTurn = document.createElement("p");
+  playerTurn.className = "player-turn-message"; // Change class name to match the HTML
   
   const boxArray = [box0, box1, box2, box3, box4, box5, box6, box7, box8];
   
@@ -55,101 +57,74 @@ var gameboard = (function () {
   // Append each box to the "boxArray" div
   boxArray.forEach(box => {
     boxArrayContainer.appendChild(box);
-
   });
   const resetButtonContainer = document.querySelector(".reset-button-container");
+  const playerTurnDiv = document.querySelector(".player-turn-div");
   resetButtonContainer.appendChild(resetButton);
+  playerTurnDiv.appendChild(playerTurn);
   resetButton.innerHTML = "Play Again!";
-
+  playerTurn.innerHTML = `Player 1's turn`;
+  
   // Return the boxArray so it can be accessed outside the IIFE
   return {
-    getBoxArray: function() {
+    getBoxArray: function () {
       return boxArray;
-    
     }
-  
   };
-
 })();
 
 // Now you can access the boxArray outside the IIFE
 const boxes = gameboard.getBoxArray();
 console.log(boxes); // Array of div elements (box0 to box8)
 
-
 //this is to hide and show parts of the page when play button is clicked
 const playButton = document.getElementById("play");
-playButton.addEventListener("click", showBoxArray);
+playButton.addEventListener("click", showGame);
 
-function showBoxArray(){
+function showGame(){
   const boxArrayElement = document.querySelector(".boxArray");
   boxArrayElement.style.display = "grid";
 
   const resetButton = document.querySelector(".reset-button-container");
   resetButton.style.display = "grid";
 
+  const playerTurn = document.querySelector(".player-turn-div");
+  playerTurn.style.display = "grid";
+
   const introPage = document.querySelector(".intro-page");
   introPage.style.display = "none"; 
-
-
 }
 
 // Add event listeners to all boxes with the class "box"
-const blueBoxes = document.querySelectorAll('.blue-box, .gray-box');
-blueBoxes.forEach(box => {
-  box.addEventListener("click", function() {
-    if (!box.innerHTML) { // Only allow placing symbols on empty boxes
-      box.innerHTML = currentPlayer.symbol;
-      currentPlayer = currentPlayer === player1 ? player2 : player1; // Switch players
-    }
-  });
+const placeSymbols = document.querySelectorAll('.blue-box, .gray-box');
+placeSymbols.forEach(box => {
+  box.addEventListener("click", handleClick);
 });
 
 var currentPlayer = player1;
 
-//Make it so that when a winner is declared or the board is full it wipes
+// Function to update the player turn message
+function updatePlayerTurnMessage() {
+  const playerTurnMessage = document.querySelector(".player-turn-message");
+  playerTurnMessage.innerHTML = `Player ${currentPlayer === player1 ? '1' : '2'}'s turn`;
+}
 
-//First Make it so that when the board is full it wipes
 
 function resetGame() {
   boxes.forEach(box => {
     box.innerHTML = '';
-  })
+    box.classList.remove("player1-symbol"); // Remove the player1-symbol class
+  });
+
+  updatePlayerTurnMessage(); // Update the player turn message
+  
+  placeSymbols.forEach(box => {
+    box.addEventListener("click", handleClick); // Add the click event listener again
+  });
 }
 
 const resetButton = document.querySelector("#reset-button");
 
 resetButton.addEventListener("click", function() {
   resetGame();
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* const element = document.getElementsByClassName("blue-box");
-element.addEventListener("click", myFunction);
-
-function myFunction() {
-  document.getElementsByClassName("blue-box").innerHTML = "X";
-}
-
-*/ 
+});
