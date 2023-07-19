a2e045e6c4ee66f1ad077b2b90ec3a22be15f841
jacknicosia <jackrileynicosia@yahoo.com>
2023-07-19 00:46:47 -0400

    reset button

--- script.js
+++ script.js
@@ -1,66 +1,71 @@
-// Gameboard = document.querySelector("#gameboard") 
-
-// var Gameboard = [1,2,3];
-
-
 
 // create gameboard and display controller with modules
 // create players with factories 
 
 
-/*const playerFactory = (name, icon) => {
-    const getName = () => name;
-    const getIcon = () => icon;
-    return {name, icon}; 
-  };  */
-
 // const gameboardArray = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+
+
+
+function createPlayer(name, symbol) {
+  return {
+    name: name,
+    symbol: symbol,
+    getPlayerId() {
+      return name + ' is ' + symbol;
+    },
+  };
+}
+
+let player1 = createPlayer("Player 1", "X");
+let player2 = createPlayer("Player 2", "O");
+
+console.log(player1.getPlayerId());
+console.log(player2.getPlayerId());
+
 
 
 var gameboard = (function () {
   'use strict';
-
   const box0 = document.createElement("div");
-  box0.id = "box0";
+  box0.className = "blue-box";
   const box1 = document.createElement("div");
-  box1.id = "box1";
+  box1.className = "gray-box";
   const box2 = document.createElement("div");
-  box2.id = "box2";
+  box2.className = "blue-box";
   const box3 = document.createElement("div");
-  box3.id = "box3";
+  box3.className = "gray-box";
   const box4 = document.createElement("div");
-  box4.id = "box4";
+  box4.className = "blue-box";
   const box5 = document.createElement("div");
-  box5.id = "box5";
+  box5.className = "gray-box";
   const box6 = document.createElement("div");
-  box6.id = "box6";
+  box6.className = "blue-box";
   const box7 = document.createElement("div");
-  box7.id = "box7";
+  box7.className = "gray-box";
   const box8 = document.createElement("div");
-  box8.id = "box8";
-
-
+  box8.className  = "blue-box";
+  const resetButton = document.createElement("button");
+  resetButton.id = "reset-button";
+  
   const boxArray = [box0, box1, box2, box3, box4, box5, box6, box7, box8];
-
-
   
   const boxArrayContainer = document.querySelector(".boxArray");
-
-
-
-
+  
   // Append each box to the "boxArray" div
   boxArray.forEach(box => {
     boxArrayContainer.appendChild(box);
+
   });
-  
- 
+  const resetButtonContainer = document.querySelector(".reset-button-container");
+  resetButtonContainer.appendChild(resetButton);
+  resetButton.innerHTML = "Play Again!";
 
-
-    // Return the boxArray so it can be accessed outside the IIFE
+  // Return the boxArray so it can be accessed outside the IIFE
   return {
     getBoxArray: function() {
       return boxArray;
+    
     }
   
   };
@@ -72,14 +77,7 @@ const boxes = gameboard.getBoxArray();
 console.log(boxes); // Array of div elements (box0 to box8)
 
 
-const element = document.getElementById("box4");
-element.addEventListener("click", myFunction);
-
-function myFunction() {
-  document.getElementById("box4").innerHTML = "X";
-}
-
-
+//this is to hide and show parts of the page when play button is clicked
 const playButton = document.getElementById("play");
 playButton.addEventListener("click", showBoxArray);
 
@@ -87,6 +85,71 @@ function showBoxArray(){
   const boxArrayElement = document.querySelector(".boxArray");
   boxArrayElement.style.display = "grid";
 
+  const resetButton = document.querySelector(".reset-button-container");
+  resetButton.style.display = "grid";
+
   const introPage = document.querySelector(".intro-page");
-  introPage.style.display = "none";
+  introPage.style.display = "none"; 
+
+
 }
+
+// Add event listeners to all boxes with the class "box"
+const blueBoxes = document.querySelectorAll('.blue-box, .gray-box');
+blueBoxes.forEach(box => {
+  box.addEventListener("click", function() {
+    if (!box.innerHTML) { // Only allow placing symbols on empty boxes
+      box.innerHTML = currentPlayer.symbol;
+      currentPlayer = currentPlayer === player1 ? player2 : player1; // Switch players
+    }
+  });
+});
+
+var currentPlayer = player1;
+
+//Make it so that when a winner is declared or the board is full it wipes
+
+//First Make it so that when the board is full it wipes
+
+function resetGame() {
+  boxes.forEach(box => {
+    box.innerHTML = '';
+  })
+}
+
+const resetButton = document.querySelector("#reset-button");
+
+resetButton.addEventListener("click", function() {
+  resetGame();
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* const element = document.getElementsByClassName("blue-box");
+element.addEventListener("click", myFunction);
+
+function myFunction() {
+  document.getElementsByClassName("blue-box").innerHTML = "X";
+}
+
+*/ 
